df9e43dfb60b1f84889954ab6e38ef23ecf77eb5
Chedli HAMMAMI <c.hammami@live.fr>
2022-05-17 15:30:52 +0200

    admin setup

--- models/index.js
+++ models/index.js
@@ -10,15 +10,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: "localhost", dialect: "mysql",
 })
 
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === ".js")
-    })
-    .forEach(file => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
-        db[model.name] = model
-    })
+
 
 Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
